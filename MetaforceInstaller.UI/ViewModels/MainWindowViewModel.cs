a87a38f1d04debb6c72c19694304c3d30ca3bbe2
shept <sheptikhinv@gmail.com>
2026-02-25 01:03:52 +0500

    file picking in mvvm is crazy

--- MetaforceInstaller.UI/ViewModels/MainWindowViewModel.cs
+++ MetaforceInstaller.UI/ViewModels/MainWindowViewModel.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Reflection;
+using System.Threading.Tasks;
+using System.Windows.Input;
 using Avalonia.Threading;
 using MetaforceInstaller.Core.Intefaces;
+using MetaforceInstaller.UI.Infrastructure;
 using MetaforceInstaller.UI.Logging;
 using Microsoft.Extensions.Logging;
 
@@ -12,6 +15,15 @@ public partial class MainWindowViewModel : ViewModelBase
     private readonly LogBuffer _logBuffer;
     private readonly ILogger<MainWindowViewModel> _logger;
     private readonly IAdbService _adbService;
+
+    public Interaction<FilePickerRequest, string?> PickFileInteraction { get; } = new();
+
+    public ICommand ChooseApkCommand { get; }
+    public ICommand ChooseZipCommand { get; }
+
+    public string? ApkPath { get; private set; }
+    public string? ZipPath { get; private set; }
+
 
     public string LogsText => _logBuffer.Text;
 
@@ -31,11 +43,35 @@ public partial class MainWindowViewModel : ViewModelBase
         _logBuffer.Changed += () =>
             Dispatcher.UIThread.Post(() => RaisePropertyChanged(nameof(LogsText)));
 
+        ChooseApkCommand = new AsyncCommand(ChooseApkAsync);
+        ChooseZipCommand = new AsyncCommand(ChooseZipAsync);
+
         _logger.LogInformation("MetaforceInstaller started");
     }
 
-    public MainWindowViewModel()
+    private async Task ChooseApkAsync()
     {
-        
+        ApkPath = await PickFileInteraction.HandleAsync(
+            new FilePickerRequest(
+                Title: "Choose .apk",
+                FileTypeName: "APK Files",
+                Patterns: ["*.apk"])
+        );
+
+        RaisePropertyChanged(nameof(ApkPath));
+    }
+
+    private async Task ChooseZipAsync()
+    {
+        ZipPath = await PickFileInteraction.HandleAsync(
+            new FilePickerRequest(
+                Title: "Choose .zip",
+                FileTypeName: "ZIP Files",
+                Patterns: ["*.zip"])
+        );
+    }
+
+    private MainWindowViewModel()
+    {
     }
 }
