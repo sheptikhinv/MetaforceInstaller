323ec0c54302aa12420133cbb30b0fc9d341ce7f
shept <sheptikhinv@gmail.com>
2026-02-26 02:19:12 +0500

    add pages functionality

---
+++ MetaforceInstaller.UI/ViewModels/NavigationItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MetaforceInstaller.UI.ViewModels;
+
+public class NavigationItem
+{
+    public string Icon { get; }
+    public string Title { get; }
+    public Type TargetPageType { get; }
+    
+    public NavigationItem(string icon, string title, Type targetPageType)
+    {
+        Icon = icon;
+        Title = title;
+        TargetPageType = targetPageType;
+    }
+}
